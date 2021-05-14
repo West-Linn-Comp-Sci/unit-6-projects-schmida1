8d2fc7b488cb326155538b023890460754302afd
schmida1 <83468154+schmida1@users.noreply.github.com>
2021-05-13 22:15:56 -0700

    Create assignment5.java

---
+++ Assignment #5/assignment5.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+public class assignment5
+{
+    public static void removeZeros(ArrayList<Integer> list) {
+        for(int i = 0; i < list.size();i++){
+            if(list.get(i)==0)
+            list.remove(i);
+        }
+    }
+    public int countLetters(ArrayList<String>list) {
+        int count = 0;
+        for(String a : list)
+        count+= a.length();
+        return counter;
+    }
+    public int findPostion(ArrayList<Integer> list, int keyVal){
+        int counter = 0;
+       for(int i = 0; i < list.size();i++){
+            if(list.get(i) == keyVal)
+            counter ++;
+        }
+        if(counter == 0)
+        counter = -1;
+        return counter;
+    }
+    public void parseIntoArrayList(ArrayList<String>list){
+        ArrayList<String>list2 = new ArrayList<String>();
+        for(int i = 0; i < list.size();i++){
+            list2.add(list.get(i));
+        }
+    }
+    public String findMin(ArrayList<Person> list){
+        int ageA = list.get(myAge);
+         for(int i = 0; i < list.size();i++){
+            if(list.get(myAge)>age1) 
+            ageA = myAge;
+        }
+    }
+    
+}
