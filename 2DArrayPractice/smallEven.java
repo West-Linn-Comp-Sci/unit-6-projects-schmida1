f10dd862ce8733c5e7c54986ac26a5d0363b8ff6
schmida1 <83468154+schmida1@users.noreply.github.com>
2021-05-13 20:10:55 -0700

    Create smallEven.java

---
+++ 2DArrayPractice/smallEven.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class smallEven here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class smallEven
+{
+      public static void main(String args[]){
+     int temp, size;
+     int array[] = {1, 26, 94, 52, 86, 11};
+     size = array.length;
+     for(int i = 0; i<size; i++ ){
+        for(int j = i+1; j<size; j++){
+           if(array[i]>array[j]){
+              temp = array[i];
+              array[i] = array[j];
+              array[j] = temp;
+           }
+        }
+     }
+     int x;
+     x = array[0] %2;
+     if(x==0){
+     System.out.println("Smallest number in the array is: "+array[0]);
+   }
+ }
+}
