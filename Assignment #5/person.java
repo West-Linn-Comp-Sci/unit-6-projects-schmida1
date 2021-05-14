f810184563d73a5de01416b82645bba6029d790e
schmida1 <83468154+schmida1@users.noreply.github.com>
2021-05-13 22:37:15 -0700

    Create person.java

---
+++ Assignment #5/person.java
@@ -0,0 +1,14 @@
+public class Person
+{
+    // instance variables - replace the example below with your own
+    private String myAge;
+    private String myName;
+    
+    public Person(String myAge1, String myName1)
+    {
+        myAge = myAge1;
+        myName = myName1;
+    }
+
+    
+}
