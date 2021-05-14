696a2c08f1dbfe240a90e295317f578d411a5f2e
schmida1 <83468154+schmida1@users.noreply.github.com>
2021-05-13 22:41:02 -0700

    Update biggestRow.java

--- 2DArrayPractice/biggestRow.java
+++ 2DArrayPractice/biggestRow.java
@@ -1,1 +1,22 @@
+public static int biggestRow(int[][] nums){
+ int numRows = nums.length;
+ int numCols = nums[0].length; 
+ int[] rowSum = new int[numRows]; 
+ int sum = 0;
+ for(int i = 0; i < numRows; i++) {
+     for(int j =0; j < numCols; j++) {
+          sum += nums[i][j]; 
+        }
+        rowSum[i] = sum; 
+    }
+int rowMax = rowSum[0];
+int maxIndex = 0;
+for(int i = 0; i < rowSum.length; i++) {
+    if (rowSum[i]> rowMax) {
+        rowMax = rowSum[i];
+        maxIndex = i; }
+}
+return maxIndex;
+}
 
+}
